5416076713262f9ee3551351610c37c18aae1118
Martin Dutra <martindsq@hotmail.com>
2019-12-20 19:35:06 -0300

    Scope event list to last week-following year

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,9 @@
 class EventsController < ApplicationController
   
   def index
-    @events = Event.where(time: (1.day.ago..1.year.from_now)).order(:time)
+    current = DateTime.current.change(:offset => -3)
+    reference = current.beginning_of_week(:monday).beginning_of_day
+    @events = Event.where(time: (1.week.ago(reference)..1.year.since(reference))).order(:time)
   end
   
   def show
