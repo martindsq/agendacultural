bd4b59036a415f4731e1b517225cc08c1b89f73d
Martin Dutra <martindsq@hotmail.com>
2019-12-20 21:50:49 -0300

    Set page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def page_title
+    if content_for?(:page_title)
+      # allows the title to be set in the view by using t(".title")
+      content_for :page_title
+    else
+      # look up translation key based on controller path, action name and .title
+      # this works identical to the built-in lazy lookup
+      t("#{ controller_path.tr('/', '.') }.#{ action_name }.title", default: :site_name)
+    end
+  end
+
 end
